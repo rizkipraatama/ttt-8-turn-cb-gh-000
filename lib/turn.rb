65b298df90b5cba957a68b21b05fd04be4f94ab8
Rizki Pratama <rizkipratama0602@gmail.com>
2019-08-20 18:21:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,8 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, input)
-  index = move(input);
+def valid_move?(board, index)
   if index > 8 || index < 0
     return false
   elsif position_taken?(board, input) == true
